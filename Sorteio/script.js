093dab3e08c4d961ca2088bb1bfa03d14f1b4575
Evandro-Prog <evandro.presenci@gmail.com>
2025-03-25 21:31:55 -0300

    .
    Adição de logica que impede que o valor1 seja menor que o valor 2

--- Sorteio/script.js
+++ Sorteio/script.js
@@ -5,11 +5,11 @@ function generateNumber() {
 
     const min = Math.ceil(document.querySelector(".input-min").value)
     const max = Math.floor(document.querySelector(".input-max").value)
-
     const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
-    alert(result)
-
-    
+    if (min >= max) {
+        alert("O valor 1 não pode ser maior ou igual ao valor 2")
+    } else {
+        alert(result)
+    }
 }
-  
